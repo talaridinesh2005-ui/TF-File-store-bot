83b6d3368157c5938826cf574477b975c4f78d9c
talaridinesh2005-ui <talaridinesh2005@gmail.com>
2025-11-21 13:04:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot Can't Export Invite link From Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double Check The FORCE_SUB_CHANNEL_1 Value And Make Sure Bot Is Admin In Channel With Invite Users Via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL_1}")
-                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/tech_freak_tamil for Support")
+                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/Team_TD_Links for Support")
                 sys.exit()
 
         if FORCE_SUB_CHANNEL_2:
@@ -54,7 +54,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL_2 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL_2}")
-                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/tech_freak_tamil for support")
+                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/Team_TD_Links for support")
                 sys.exit()
         if FORCE_SUB_CHANNEL_3:
             try:
@@ -67,7 +67,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot Can't Export Invite link From Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double Check The FORCE_SUB_CHANNELS Value And Make Sure Bot Is Admin In Channel With Invite Users Via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL_3}")
-                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/tech_freak_tamil For Support")
+                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/Team_TD_Links For Support")
                 sys.exit()
 
         if FORCE_SUB_CHANNEL_4:
@@ -81,7 +81,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot Can't Export Invite link From Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double Check The FORCE_SUB_CHANNEL Value And Make Sure Bot Is Admin In Channel With Invite Users Via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL_4}")
-                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/tech_freak_tamil For Support")
+                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/Team_TD_Links For Support")
                 sys.exit()
 
         try:
@@ -92,7 +92,7 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure Bot Is Admin In DB Channel, And Double Check The CHANNEL_ID Value, Current Value: {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. https://t.me/tech_freak_tamil For Support")
+            self.LOGGER(__name__).info("\nBot Stopped. https://t.me/Team_TD_Links For Support")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
